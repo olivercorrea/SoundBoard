13abaca7eef9bacf4a8ddd768e86d6b50a666b1d
Oliver Correa Cabana <oliver@Olivers-iMac-Pro.local>
2024-05-21 22:06:02 -0500

    first commit

---
+++ SoundBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SoundBoard
+//
+//  Created by Oliver Correa Cabana on 21/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
